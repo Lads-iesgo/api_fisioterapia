27c8423ed59e86debaf43489a7e15169c82a87f0
Halbert Nascimento <halbert.fac@gmail.com>
2025-05-15 10:24:04 -0300

    Adicionar novas rotas para gerenciamento de horários de agendamento

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -3,14 +3,16 @@ import express, { Request, Response, NextFunction } from "express";
 import userRoutes from "./user.routes";
 import clientRoutes from "./paciente.routes";
 import perfilRoutes from "./perfil.routes";
+import horarioAgendamentoRoutes from "./horarioAgendamento.routes"; // Importe as novas rotas
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/paciente", clientRoutes);
 app.use("/usuario", userRoutes);
+app.use("/paciente", clientRoutes);
 app.use("/perfil", perfilRoutes);
+app.use("/horario-agendamento", horarioAgendamentoRoutes); // Use as novas rotas
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
